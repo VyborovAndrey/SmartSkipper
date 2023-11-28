ec0291c5be1f25b1110382d064b33b1490318703
Группа 4215 Бевз Л.А <leonidbevz666@gmail.com>
2023-11-28 22:01:08 +0300

    Создан алгоритм генерации путей для лодок в гонке

--- coursework/Smartskipper/createGPX.py
+++ coursework/Smartskipper/createGPX.py
@@ -3,11 +3,12 @@ import csv
 
 #list = [["latitude","longitude","time"],[30,60,"время"]] - формат списка
 
-def create_gpx(list):
-    with open('out.csv', 'w', newline='') as f:
+def create_gpx(list: list, name: str):
+    list.insert(0, ["latitude", "longitude", "time"])
+    with open(f"{name}.csv", 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(list)
-    Converter(input_file="out.csv").csv_to_gpx(lats_colname='latitude', longs_colname='longitude', times_colname='time', output_file='your_input.gpx')
+    Converter(input_file=f"{name}.csv").csv_to_gpx(lats_colname='latitude', longs_colname='longitude', times_colname='time', output_file=f'{name}.gpx')
 
 
 
